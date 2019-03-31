afae8a9a5caf597942de9ece72b0f0cc5cc25fde
MineralwaterXu <mineralwater.xu@gmail.com>
2019-03-31 19:30:44 +0800

    daily update

---
+++ c++primer/chapter3/exp3.1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//using declaration, when we use the name "cin", get it from the name space "std"
+using std::cin;
+
+int main()
+{
+    int i;
+    cin >> i;
+    std::cout << i;
+    return 0;
+}
