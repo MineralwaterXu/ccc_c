ff0252d4ec14453c262e2ed12e7c7aaf764a0c3d
MineralwaterXu <mineralwater.xu@gmail.com>
2019-04-01 19:51:33 +0800

    daily update

---
+++ c++primer/chapter3/exp3.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using std::string;
+
+string s1;
+string s2 = s1;
+string s3 = "hiya";
+string s4(10, 'c');
+string s5 = "hiya";
+string s6("hiya");
+string s7(10, 'c');
+string s8 = string(10, 'c');
+string temp(10, 'c');
+string s9 = temp;
