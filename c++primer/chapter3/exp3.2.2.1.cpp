65e69b4a457983691b4828fdfa337b5756e298ea
MineralwaterXu <mineralwater.xu@gmail.com>
2019-04-03 21:57:24 +0800

    daily update

--- c++primer/chapter3/exp3.2.2.1.cpp
+++ c++primer/chapter3/exp3.2.2.1.cpp
@@ -7,10 +7,10 @@ using std::string;
 int main()
 {
     string s; //empty string
-    cin >> s; //put s into string until null
-    cout << s << endl; //input s
+    cin >> s; //put string into s until null
+    cout << s << endl; //output s
     string s1, s2;
-    cin >> s1 >> s2;
-    cout << s1 << s2 << endl;
+    cin >> s1 >> s2; //put first into s1, second into s2
+    cout << s1 << s2 << endl; //output two strings
     return 0;
 }
